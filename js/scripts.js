e27b85de5ebf9bcd4fa9e65c8bb2ec044a25afc0
gstrong2 <gary.strong21@yahoo.com>
2022-02-14 15:16:28 -0700

    Updates
    Updated jQuery and CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log("Hello! Welcome to my portfolio")
+function menuToggle() {
+   var x = document.getElementById('myNavtoggle');{
+   x.className += ' responsive';
+ } else {
+  x.className = 'navtoggle';
+}
+}
